a699a06475a7e49d2e776626438a9820a2fb1f62
Sal Castoro <20549092+scastoro@users.noreply.github.com>
2022-03-15 15:15:08 -0400

    Practice Node.js site

---
+++ reference/os_demo.js
@@ -0,0 +1,18 @@
+const os = require("os");
+
+// Platform
+console.log(os.platform());
+
+// CPU Arch
+console.log(os.arch());
+
+// CPU Core info
+// console.log(os.cpus());
+
+// Free memory
+console.log(os.freemem());
+
+// Free memory
+console.log(os.totalmem());
+
+
---
+++ reference/path_demo.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+// Base file name
+console.log(path.basename(__filename));
+
+// Directory Name
+console.log(path.dirname(__filename));
+
+// File extension
+console.log(path.extname(__filename));
+
+// Create path object
+console.log(path.parse(__filename).base);
+
+// Concatenate paths
